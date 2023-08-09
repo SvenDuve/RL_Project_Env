f619a4ea0c2dda87e966545666aa184f8abd7413
Sven Duve <svenduve@gmail.com>
2023-08-09 16:52:41 +0200

    update demo agentparams

--- demo_DDPG.jl
+++ demo_DDPG.jl
@@ -11,5 +11,5 @@ aP = agent(Pendulum(), AgentParameter(training_episodes=500))
 println("Training Complete, now testing for 10 Episodes...")
 
 for i in 1:10
-    renderEnv(Pendulum(), aP.trained_agents[end])
+    renderEnv(Pendulum(), aP.trained_agents[end], rand(1:2000))
 end
