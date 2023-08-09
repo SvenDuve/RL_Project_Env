299d3cd0ef81b7ac9680e3334b4743af03b46bd9
Sven Duve <svenduve@gmail.com>
2023-08-09 16:29:41 +0200

    added demo

--- demo_DDPG.jl
+++ demo_DDPG.jl
@@ -1,4 +1,15 @@
+println("Loading Packges...")
+
 using DDPG
 using RLTypes
 
-agent(Pendulum(), AgentParameter(training_episodes=200))
+println("Training Agent for 500 Episodes, please wait...")
+
+aP = agent(Pendulum(), AgentParameter(training_episodes=500))
+
+
+println("Training Complete, now testing for 10 Episodes...")
+
+for i in 1:10
+    renderEnv(Pendulum(), aP)
+end
