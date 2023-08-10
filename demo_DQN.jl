5b4b31019447e1ae913ee3b8dad4c746bf028740
Sven Duve <svenduve@gmail.com>
2023-08-10 19:15:00 +0200

    added render DQN

--- demo_DQN.jl
+++ demo_DQN.jl
@@ -8,8 +8,8 @@ println("Training Agent for 500 Episodes, please wait...")
 aP = DQN.agent(LunarLanderDiscrete(), AgentParameter(training_episodes=500))
 
 
-# println("Training Complete, now testing for 10 Episodes...")
+println("Training Complete, now testing for 10 Episodes...")
 
-# for i in 1:10
-#     renderEnv(LunarLanderDiscrete(), aP.trained_agents[end], rand(1:2000))
-# end
+for i in 1:10
+    DQN.renderEnv(LunarLanderDiscrete(), aP.trained_agents[end], rand(1:2000))
+end
