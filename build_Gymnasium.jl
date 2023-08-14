83d3fb7ebf82029ec6ec37fd7b101ceabf9a902d
Sven Duve <svenduve@gmail.com>
2023-08-14 13:48:58 +0200

    adjusted install script

--- build_Gymnasium.jl
+++ build_Gymnasium.jl
@@ -5,10 +5,11 @@ using Conda
 using PyCall
 
 Conda.pip_interop(true)
-Conda.pip("install", "git+https://github.com/SvenDuve/Gymnasium")
 Conda.add("wheel")
+println("Done installing wheel")
 Conda.add("box2d-py")
+println("Done installing box2d-py")
 Conda.add("pygame")
-
-
+println("Done installing pygame")
+Conda.pip("install", "git+https://github.com/SvenDuve/Gymnasium")
 println("Gymnasium installed")
